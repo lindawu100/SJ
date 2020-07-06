63b7207b561d79c67d5e22b7b3809dba852c0e69
Linda Wu <pinky510100@gmail.com>
2020-07-06 18:17:03 +0800

    show alert if amount exceed balance

--- app/controllers/api/v1/wallet_controller.rb
+++ app/controllers/api/v1/wallet_controller.rb
@@ -1,18 +1,22 @@
 class Api::V1::WalletController < ApplicationController
 
   def transaction
-    
     owner_wallet = Wallet.find(params[:id])
     receiver_wallet = Wallet.find(params[:deal][:receiverid])
     amount = params[:deal][:amount].to_i
+  
+    if owner_wallet.balance >= amount
+      owner_wallet.update(balance: owner_wallet.balance -= amount)
 
-    owner_wallet.update(balance: owner_wallet.balance -= amount)
+      receiver_wallet.update(balance: receiver_wallet.balance += amount)
 
-    receiver_wallet.update(balance: receiver_wallet.balance += amount)
+      Deal.create(ownerid: params[:id], receiverid: params[:deal][:receiverid], amount: params[:deal][:amount])
 
-    Deal.create(ownerid: params[:id], receiverid: params[:deal][:receiverid], amount: params[:deal][:amount])
+      redirect_to wallet_path(owner_wallet.id)
+    else
+      redirect_to wallet_path(owner_wallet.id), alert: "Amount of transaction can not exceed balance." 
+    end
 
-    redirect_to wallet_path(owner_wallet.id)
   end
 
 end
