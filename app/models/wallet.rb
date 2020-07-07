b80c47f4552073a56383ab85ceaf915cd465667d
Linda Wu <pinky510100@gmail.com>
2020-07-07 15:59:01 +0800

    use rails transaction

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,13 @@
 class Wallet < ApplicationRecord
   has_many :deal, foreign_key: 'ownerid'
   has_many :deal, foreign_key: 'receiverid'
+
+  def withdraw(amount)
+    self.update!(balance: self.balance - amount)
+  end
+
+  def deposit(amount)
+    self.update!(balance: self.balance + amount)
+  end
+
 end
