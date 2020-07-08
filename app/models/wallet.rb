624a07892292053fb60882e771c44cdf05594b73
Linda Wu <pinky510100@gmail.com>
2020-07-08 23:28:26 +0800

    test for deal.rb

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,16 +1,14 @@
 class Wallet < ApplicationRecord
-  has_many :deal, foreign_key: 'ownerid'
-  has_many :deal, foreign_key: 'receiverid'
 
   def withdraw(amount)
     if amount > 0 && amount <= self.balance
-      self.update!(balance: self.balance - amount)
+      self.update!(balance: self.balance - amount)   
     end
   end
 
   def deposit(amount)
     if amount > 0
-      self.update!(balance: self.balance + amount)
+      self.update!(balance: self.balance + amount)   
     end
   end
 
